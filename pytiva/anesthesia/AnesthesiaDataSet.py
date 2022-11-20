00fed2c4a79d10563ad53de0efcaddd4484dd580
TJ Biel <tjbiel85@gmail.com>
2022-11-20 14:23:13 -0800

    - Updated AnesthesiaDataSet:   - added a method _with_timespans(), which returns a version of - Updated AnesthesiaCaseMedicationsDataSet:   - specified some required columns (enforced through DataSet definitions)   - added method to_activity_dataset(), a helper to create an ActivityDataSet object from medication data, including applying a time offset before and after in order to generate a time span using the AnesthesiaDataSet._with_timespans() method - Updated ActivityDataSet:   - moved some default method arguments to be assigned to an object attribute   - added method _generate_stratified_df_slices(), which uses a supplied collection of column names to make a generator yielding slices of the data, each with a unique combination of the values in the named columns.   - added method fetch_unduplicated_concurrency(), which will consolidate overlapping activity across the entire ActivityDataSet and can be filtered by activities and stratified by column names - Added test_activity_unduplication_with_meds.py, which checks most of the above functionality, and some related data in csv files

--- pytiva/anesthesia/AnesthesiaDataSet.py
+++ pytiva/anesthesia/AnesthesiaDataSet.py
@@ -1,5 +1,7 @@
 from ..dataset import DataSet
 
+from ..utils import datetime_to_activity_timespan
+
 
 class AnesthesiaDataSet(DataSet):
     """
@@ -8,3 +10,22 @@ class AnesthesiaDataSet(DataSet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def _with_timespans(self,
+                        datetime_col,
+                        td_offset_before,
+                        td_offset_after):
+        """
+        Return a version of this DataSet with start and stop times, based on admin time plus supplied offsets.
+        :param offset_before:
+        :param offset_after:
+        :return:
+        """
+
+        return datetime_to_activity_timespan(
+            self,
+            datetime_col=datetime_col,
+            td_before=td_offset_before,
+            td_after=td_offset_after,
+            inplace=False
+        )
--- pytiva/utils/utils.py
+++ pytiva/utils/utils.py
@@ -8,13 +8,18 @@ def hash_and_slash(x, slice_size=200):
     slice_end = slice_offset + slice_size
     hashed = hashlib.sha256(str(x).encode('utf-8')).hexdigest()
     hashed = hashed + hashed
-    return hashed[:slice_size]
+    return hashed[slice_offset:slice_size]
 
 
 def hash_cols_in_df(df, hash_func, cols):
+    """
     # hash these columns, obfuscating their contents
     # but do so in a fashion that makes the same hash for each input value
-    # is that called "in a deterministic fashion?"
+    :param df: DataFrame
+    :param hash_func:
+    :param cols:
+    :return: a hashed version of the DataFrame
+    """
     df_out = df.copy()
 
     for h in cols:
@@ -46,3 +51,30 @@ def timedelta_offset_cols_in_df(df, td_func, cols):
             df_out.loc[i, c] = df_out.loc[i, c] + offset
 
     return df_out
+
+
+def datetime_to_activity_timespan(df, datetime_col, td_before, td_after,
+                                  before_out='activity_start',
+                                  after_out='activity_end',
+                                  inplace=True):
+    """
+    Helper function to convert a single datetime time point to an activity time span
+    i.e. to generate activity_start (before the datetime) and an activity_end (after
+    the datetime).
+
+    params:
+    df is a pandas DataFrame object
+    datetime_col is the column to be used as the single datetime time point
+    td_before and td_after should be timedelta objects
+
+    returns a DataFrame object with the additional columns, or a copy of the same
+    if inplace is False
+
+    """
+    if not inplace:
+        df = df.copy()
+
+    df[before_out] = df[datetime_col].apply(lambda x: x - td_before)
+    df[after_out] = df[datetime_col].apply(lambda x: x + td_after)
+
+    return df
